a9f1cee7373a5222c692660602f93d1aee90cf8e
JOE-DP <joe.purvis@hotmail.co.uk>
2022-05-04 16:53:51 +0100

    heroku bug testing

--- server.js
+++ server.js
@@ -5,36 +5,36 @@ const http = require('http')
 const app = express()
 
 //mongo DB set up
-const MongoClient = require('mongodb').MongoClient
-let db
-let dbConnectionStr = process.env.DB_STRING
-let dbName = 'todolist'
+// const MongoClient = require('mongodb').MongoClient
+// let db
+// let dbConnectionStr = process.env.DB_STRING
+// let dbName = 'todolist'
 
-MongoClient.connect(dbConnectionStr)
-    .then(client => {
-        console.log(`connected to ${dbName} database`)
-        db = client.db(dbName)
-    })
+// MongoClient.connect(dbConnectionStr)
+//     .then(client => {
+//         console.log(`connected to ${dbName} database`)
+//         db = client.db(dbName)
+//     })
 
 app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
+// app.use(express.static('public'))
+// app.use(express.urlencoded({extended: true}))
+// app.use(express.json())
 
 app.get('/', (req, res) => 
     db.collection('todo').find().toArray()
         .then(data => res.render('index.ejs', {list: data} ))
  )
 
- app.post('/addListItem', (req, res) => {
-    db.collection('todo').insertOne({'listItem': req.body.listItem, 'completed': false})
-    res.redirect('/')
- })
+//  app.post('/addListItem', (req, res) => {
+//     db.collection('todo').insertOne({'listItem': req.body.listItem, 'completed': false})
+//     res.redirect('/')
+//  })
 
- app.delete('/deleteItem', (req, res) => {
-     db.collection('todo').deleteOne({listItem: req.body.deleteItem.trim()})
-        .then(() => res.json())
- })
+//  app.delete('/deleteItem', (req, res) => {
+//      db.collection('todo').deleteOne({listItem: req.body.deleteItem.trim()})
+//         .then(() => res.json())
+//  })
 
 
 const PORT = 5002
